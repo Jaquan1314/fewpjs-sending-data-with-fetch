fe418a0af0396f38d6fd4d48b1df1b324c3b01cd
Jaquan1314 <javaughngrant@gmail.com>
2020-11-19 16:27:52 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(resp => resp.json())
+  .then(data => {
+    let id = data.id
+    document.body.append(id)
+  })
+  .catch(err => {
+    let error = err.message
+    document.body.append(error)
+  })
+}
+// submitData("javaughn", "test@test")
